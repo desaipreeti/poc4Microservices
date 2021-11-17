6a8b2cddb579c127b8eeb71a2435fd2cb09f8896
desaipreeti <desaipreetia92@gmail.com>
2021-11-17 18:19:07 +0530

    initial commit

---
+++ poc4microservice2/src/main/java/com/neosoft/poctask/Poc4microservice2Application.java
@@ -0,0 +1,15 @@
+package com.neosoft.poctask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc4microservice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc4microservice2Application.class, args);
+		
+		System.out.println("microservice2");
+	}
+
+}
